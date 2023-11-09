84928e8e779ec421091cb57c87b30651bf8baf50
Ioannis Plakas <iplakas@nubificus.co.uk>
2023-11-09 14:19:15 +0200

    Add updates
    Signed-off-by: Ioannis Plakas <iplakas@nubificus.co.uk>

--- native-hello-world/hello.go
+++ native-hello-world/hello.go
@@ -1,28 +1,46 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
-  "os"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("On my way to unikernels!!!")
-	fmt.Fprintf(w, "Hello--unifan !\n")
 
+	// Get the hostname of the local machine
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Printf("Error getting hostname: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Resolve the hostname to an IP address
+	addrs, err := net.LookupHost(hostname)
+	if err != nil {
+		fmt.Printf("Error resolving hostname to IP: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Print the first IP address found (assuming it's the primary one)
+	fmt.Printf("Node IP: %s\n", addrs[0])
+
+	fmt.Fprintf(w, "Hello ! The  IP  is : "+addrs[0]+"\n")
 }
 
 func main() {
-  log.Print("helloworld: starting server...")
+	log.Print("helloworld: starting server...")
 
-  http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
 
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "8080"
-  }
+	port := os.Getenv("C_PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-  log.Printf("helloworld: listening on port %s", port)
-  log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	log.Printf("helloworld: listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
